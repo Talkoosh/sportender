682e8b8692a969b726ef3a03b8003cb2b27090f5
TalSB <artishok.tal@gmail.com>
2022-11-11 12:57:20 +0200

    base work on main page bottom nav

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import {View } from 'react-native';
 import Login from './src/views/Login';
 import genericStyles from './src/style/generic';
+import BottomNav from './src/components/BottomNav';
 
 export default function App() {
     return (
         <View style={genericStyles.pageContainer}>
-            <Login></Login>
+            {/* <Login /> */}
+            <BottomNav />
         </View>
     );
 }
--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { Dimensions } from 'react-native';
 import genericStyles from '../style/generic';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons/faMapLocationDot';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { faStar } from '@fortawesome/free-regular-svg-icons/faStar';
 
 const BottomNav = () => {
     return (
         <View style={navStyles.navContainer}>
-            <Text>BottomNav</Text>
+            <View style={navStyles.iconsContainer}>
+                <TouchableOpacity>
+                    <FontAwesomeIcon icon={faBars} size={22} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <FontAwesomeIcon icon={faStar} size={22} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <FontAwesomeIcon icon={faMapLocationDot} size={22} color="black" />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
@@ -14,8 +29,17 @@ export default BottomNav;
 
 const navStyles = StyleSheet.create({
     navContainer: {
-        borderWidth: 1,
-        width: '100%',
-        height: '100%',
+        borderTopWidth: 1,
+        position: 'absolute',
+        bottom: 0,
+        width: Dimensions.get('window').width,
+        height: 100,
+        backgroundColor: genericStyles.colors.cream,
+    },
+    iconsContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
     },
 });
